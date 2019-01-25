84ff87931202b1eaf634dd1c883a637ff0c1c787
Claudio Dietrich <crdietrichjr@gmail.com>
2019-01-25 17:50:09 -0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Intro/>
+  </div>
+</template>
+
+<script>
+import Intro from '@/components/Intro.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Intro
+  }
+}
+</script>
+
+<style>
+.title {
+  font-family: 'Montserrat', sans-serif;
+}
+
+.subtile {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero has-text-centered">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-size-4-mobile is-size-1-desktop">Full Stack Developer</h1>
+        <h2 class="subtitle is-size-5-mobile is-size-4-desktop">I code beautifully simple things, and I love what I do.</h2>
+        <img class="is-bottom" src="@/assets/img/avatar.svg" alt="Avatar">
+      </div>
+    </div>
+
+    <div class="hero-foot">
+      <div class="container">
+        <img src="@/assets/img/hero.png" alt="Hero">
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+@media screen and (min-width: 769px) {
+  .hero-foot img {
+    max-width: 860px;
+  }
+}
+</style>
+
